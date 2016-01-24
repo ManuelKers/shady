8d411bc3ffe94dfdee1901779ac8028631b36443
Manuel Kerssemakers <manuel@abbeygames.com>
2016-01-24 21:55:38 +0100

    Added MusicPlayer

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioSource source;
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		if (!source.isPlaying) {			
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+			
+	}
+}
